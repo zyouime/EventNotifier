7436fcd3af85466563b7fedd9f17c9ee8c445f42
zyouime <igorneizvestny69@gmail.com>
2025-05-17 16:49:06 +0300

    labymodfix

--- src/main/java/me/zyouime/eventnotifier/mixin/ChatScreenMixin.java
+++ src/main/java/me/zyouime/eventnotifier/mixin/ChatScreenMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChatScreen.class)
+@Mixin(value = ChatScreen.class, priority = 500)
 public abstract class ChatScreenMixin extends Screen {
 
     @Unique
